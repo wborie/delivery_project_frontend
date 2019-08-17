fc4a4564d2b3913881788375bfd9424efe59999a
Will Borie <borie@usc.edu>
2019-08-17 11:05:10 -0700

    Finished map creation pageLocation Add/Edit/Delete

--- src/components/CreateMapPage.js
+++ src/components/CreateMapPage.js
@@ -49,7 +49,7 @@ const FormTitle = styled.h3`
 `;
 
 const FormItem = styled.div`
-	display: flex;
+	display: ${props => (props.display === `none`)? `none` : `flex`};
 	flex-wrap: wrap;
 `;
 
@@ -58,6 +58,7 @@ const InputDescription = styled.p`
 `;
 
 const InputText = styled.p`
+	color: ${props => props.color ? props.color : `white`};
 	margin: auto 5px auto 5px;
 `;
 
@@ -71,6 +72,7 @@ const FormInput = styled.input`
 	border-radius: 10px;
 	border-style: dashed;
 	padding: 8px;
+	height: 14px;
 	:focus {
 		outline: none;
 		border-color: yellow;
@@ -108,9 +110,10 @@ class CreateMapPage extends Component {
 			currentRoadEndY: null,
 			currentRoadName: null,
 			currentRoadWeight: null,
-			currentLocationX: null,
-			currentLocationY: null,
+			currentLocationX: -1,
+			currentLocationY: -1,
 			currentLocationRoadName: null,
+			currentLocationRoadNameValid: false,
 			currentLocationName: null,
 			canvas: null,
 			context: null,
@@ -119,9 +122,13 @@ class CreateMapPage extends Component {
 			locations: new Map()
 		};
 		this.handleInsertRoadSubmit = this.handleInsertRoadSubmit.bind(this);
-		this.handleInsertLocationSubmit = this.handleInsertLocationSubmit.bind(this);
-		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleRoadInputChange = this.handleRoadInputChange.bind(this);
 		this.handleDeleteRoad = this.handleDeleteRoad.bind(this);
+		this.handleLocationInputChange = this.handleLocationInputChange.bind(this);
+		this.increaseLocationCoordinate = this.increaseLocationCoordinate.bind(this);
+		this.decreaseLocationCoordinate = this.decreaseLocationCoordinate.bind(this);
+		this.handleInsertLocationSubmit = this.handleInsertLocationSubmit.bind(this);
+		this.handleDeleteLocation = this.handleDeleteLocation.bind(this);
 	}
 
 	componentDidMount() {
@@ -192,13 +199,33 @@ class CreateMapPage extends Component {
 
 	handleInsertLocationSubmit(event) {
 		event.preventDefault();
+		if (this.inputsContainNull([this.state.currentLocationRoadName, this.state.currentLocationName])) {
+			return;
+		}
+		let newLocation = {
+			x: Number(this.state.currentLocationX),
+			y: Number(this.state.currentLocationY),
+			name: this.state.currentLocationName,
+			roadName: this.state.currentLocationRoadName
+		};
+
+		this.state.locations.set(this.state.currentLocationRoadName + "-" + this.state.currentLocationName, newLocation)
+		this.setState({currentLocationX: -1, currentLocationY: -1, currentLocationName: null, currentLocationRoadName: null,
+			currentLocationRoadNameValid: false}, () => {
+			this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations)
+		});
+
+		// Clear the currently displayed inputs
+		this.clearLocationForm();
+
 	}
 
-	handleInputChange(event) {
+	handleRoadInputChange(event) {
 		const updatedStateObject = {};
 		updatedStateObject[event.target.name] = event.target.value;
 
 		if (event.target.name === "currentRoadName" && this.state.roads.has(event.target.value)) {
+			// An existing road is being edited
 			const roadObject = this.state.roads.get(event.target.value);
 			this.setRoadForm(roadObject.startX, roadObject.startY, roadObject.endX, roadObject.endY, roadObject.name, roadObject.weight);
 			updatedStateObject["currentRoadStartX"] = roadObject.startX;
@@ -221,7 +248,7 @@ class CreateMapPage extends Component {
 	drawCanvas(canvas, context, roads, locations) {
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		this.drawRoads(canvas, context, roads);
-		//this.drawLocations(canvas, context, locations)
+		this.drawLocations(canvas, context, locations)
 	}
 
 	drawRoads(canvas, context, roads) { // roads is [{startX: x, startY: y, endX: x, endY: y, name: z, weight: w, editing: true/false}]
@@ -247,15 +274,40 @@ class CreateMapPage extends Component {
 		})
 	}
 
+	drawLocations(canvas, context, locations) { // locations is [{x, y, name}]
+		locations.forEach(location => {
+			context.beginPath();
+			context.arc(location.x, location.y, 3, 0, 2 * Math.PI);
+			context.fill();
+			context.stroke();
+		})
+	}
+
 	handleDeleteRoad(event) {
 		event.preventDefault();
 		this.state.roads.delete(this.state.currentRoadName);
+		this.state.locations.forEach((location, key) => {
+			if (location.roadName === this.state.currentRoadName) {
+				this.state.locations.delete(key);
+			}
+		})
 		this.setState({currentRoadStartX: null, currentRoadStartY: null, currentRoadEndX: null, currentRoadEndY: null, 
 			currentRoadName: null, currentRoadWeight: null}, () => {
-			this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations)
+			this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations);
 		});
 		// Clear the currently displayed inputs
 		this.clearRoadForm(false);
+	}
+
+	handleDeleteLocation(event) {
+		event.preventDefault();
+		this.state.locations.delete(this.state.currentLocationRoadName + "-" + this.state.currentLocationName);
+		this.setState({currentLocationX: -1, currentLocationY: -1, currentLocationName: null, currentLocationRoadName: null,
+			currentLocationRoadNameValid: false}, () => {
+			this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations);
+		});
+		// Clear the currently displayed inputs
+		this.clearLocationForm();
 	}
 
 	setRoadForm(startX, startY, endX, endY, name, weight) {
@@ -276,6 +328,96 @@ class CreateMapPage extends Component {
 		document.getElementsByName("currentRoadWeight")[0].value = "";
 	}
 
+	clearLocationForm() {
+		document.getElementsByName("currentLocationName")[0].value = "";
+		document.getElementsByName("currentLocationRoadName")[0].value = "";
+	}
+
+	handleLocationInputChange(event) {
+		const updatedStateObject = {};
+		updatedStateObject[event.target.name] = event.target.value;
+
+		// If location is being inserted
+		if (event.target.name === "currentLocationRoadName" && this.state.roads.has(event.target.value)) {
+			updatedStateObject["currentLocationRoadNameValid"] = true;
+			const locationRoad = this.state.roads.get(event.target.value);
+			updatedStateObject["currentLocationX"] = locationRoad.startX + ((locationRoad.endX - locationRoad.startX) / 2);
+			updatedStateObject["currentLocationY"] = locationRoad.startY + ((locationRoad.endY - locationRoad.startY) / 2);
+		} else if (event.target.name === "currentLocationRoadName" && !this.state.roads.has(event.target.value)) {
+			updatedStateObject["currentLocationRoadNameValid"] = false;
+		}
+
+		// If location is being edited
+		if (event.target.name === "currentLocationRoadName" && this.state.currentLocationName !== null) {
+			const locationID = event.target.value + "-" + this.state.currentLocationName;
+			if (this.state.locations.has(locationID)) {
+				updatedStateObject["currentLocationX"] = this.state.locations.get(locationID).x;
+				updatedStateObject["currentLocationY"] = this.state.locations.get(locationID).y;
+			}
+		} else if (event.target.name === "currentLocationName" && this.state.currentLocationRoadName !== null) {
+			const locationID = this.state.currentLocationRoadName + "-" + event.target.value;
+			if (this.state.locations.has(locationID)) {
+				updatedStateObject["currentLocationX"] = this.state.locations.get(locationID).x;
+				updatedStateObject["currentLocationY"] = this.state.locations.get(locationID).y;
+			}
+		}
+
+		// If location was just being edited
+		if ((event.target.name === "currentLocationName" || event.target.name === "currentLocationRoadName") &&
+			this.state.locations.has(this.state.currentLocationRoadName + "-" + this.state.currentLocationName)) {
+			const locationRoad = this.state.roads.get((event.target.name === "currentLocationRoadName") ? event.target.value :
+				this.state.currentLocationRoadName);
+			updatedStateObject["currentLocationX"] = locationRoad.startX + ((locationRoad.endX - locationRoad.startX) / 2);
+			updatedStateObject["currentLocationY"] = locationRoad.startY + ((locationRoad.endY - locationRoad.startY) / 2);
+		}
+
+		this.setState(updatedStateObject);
+	}
+
+	increaseLocationCoordinate(event) {
+		const currentRoad = this.state.roads.get(this.state.currentLocationRoadName);
+		const dx = currentRoad.endX - currentRoad.startX;
+		const dy = currentRoad.endY - currentRoad.startY;
+		const newX = this.state.currentLocationX + (dx / 100); // change the 1 to dx/100
+		const newY = (dx !== 0) ? this.state.currentLocationY + ((dx / 100) * (dy / dx)) : this.state.currentLocationY + (dy / 100);
+
+		// let newLocation = {
+		// 	x: Number(newX),
+		// 	y: Number(newY),
+		// 	name: this.state.currentLocationRoadName
+		// };
+
+		// this.state.locations.set(this.state.currentLocationRoadName + "-" + this.state.currentLocationName, newLocation)
+		if ((currentRoad.endX - newX) >= Math.min(0, dx) && (currentRoad.endX - newX) <= Math.max(0, dx) && 
+			(currentRoad.endY - newY) >= Math.min(0, dy) && (currentRoad.endY - newY) <= Math.max(0, dy)) {
+			this.setState({currentLocationX: newX, currentLocationY: newY}, () => {
+				this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations)
+			});
+		}
+	}
+
+	decreaseLocationCoordinate(event) {
+		const currentRoad = this.state.roads.get(this.state.currentLocationRoadName);
+		const dx = currentRoad.endX - currentRoad.startX;
+		const dy = currentRoad.endY - currentRoad.startY;
+		const newX = this.state.currentLocationX - (dx / 100); // change the 1 to dx/100
+		const newY = (dx !== 0) ? this.state.currentLocationY - ((dx / 100) * (dy / dx)) : this.state.currentLocationY - (dy / 100);
+
+		// let newLocation = {
+		// 	x: Number(newX),
+		// 	y: Number(newY),
+		// 	name: this.state.currentLocationRoadName
+		// };
+
+		// this.state.locations.set(this.state.currentLocationRoadName + "-" + this.state.currentLocationName, newLocation)
+		if ((currentRoad.endX - newX) >= Math.min(0, dx) && (currentRoad.endX - newX) <= Math.max(0, dx) && 
+			(currentRoad.endY - newY) >= Math.min(0, dy) && (currentRoad.endY - newY) <= Math.max(0, dy)) {
+			this.setState({currentLocationX: newX, currentLocationY: newY}, () => {
+				this.drawCanvas(this.state.canvas, this.state.context, this.state.roads, this.state.locations)
+			});
+		}
+	}
+
 	render() {
 		const roadBeingEdited = this.state.roads.has(this.state.currentRoadName);
 		const RoadFormButtons = roadBeingEdited ? 
@@ -293,6 +435,16 @@ class CreateMapPage extends Component {
 			</Button>
 		</FormItem>);
 
+		const locationBeingEdited = this.state.locations.has(this.state.currentLocationRoadName + "-" + this.state.currentLocationName);
+		const LocationFormButtons = locationBeingEdited ?
+		(<FormItem display={this.state.currentLocationRoadNameValid ? "" : "none"}>
+			<Button onClick={this.handleInsertLocationSubmit} style={{marginRight:"10px"}}>Edit Location</Button>
+			<Button onClick={this.handleDeleteLocation}>Delete Location</Button>
+		</FormItem>) :
+		(<FormItem display={this.state.currentLocationRoadNameValid ? "" : "none"}>
+			<Button onClick={this.handleInsertLocationSubmit}>Insert Location</Button>
+		</FormItem>);
+
 		return (
 			<PageContainer>
 				<InsertionContainer>
@@ -301,29 +453,27 @@ class CreateMapPage extends Component {
 							<FormTitle>Insert/Edit Road</FormTitle>
 							<FormItem style={{marginBottom:"10px"}}>
 								<InputDescription>Road Name:</InputDescription>
-								<FormInput type="text" boxWidth="200px" name="currentRoadName" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" boxWidth="200px" name="currentRoadName" onChange={this.handleRoadInputChange}></FormInput>
 							</FormItem>
 							<FormItem style={{marginBottom:"10px"}}>
 								<InputDescription>Start Coordinate (X,Y):</InputDescription>
 								<InputText>(</InputText>
-								<FormInput type="text" name="currentRoadStartX" onChange={this.handleInputChange}></FormInput>
-								<InputText>) , </InputText>
-								<InputText>(</InputText>
-								<FormInput type="text" name="currentRoadStartY" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" name="currentRoadStartX" onChange={this.handleRoadInputChange}></FormInput>
+								<InputText> , </InputText>
+								<FormInput type="text" name="currentRoadStartY" onChange={this.handleRoadInputChange}></FormInput>
 								<InputText>)</InputText>
 							</FormItem>
 							<FormItem style={{marginBottom:"10px"}}>
 								<InputDescription>End Coordinate (X,Y):</InputDescription>
 								<InputText>(</InputText>
-								<FormInput type="text" name="currentRoadEndX" onChange={this.handleInputChange}></FormInput>
-								<InputText>) , </InputText>
-								<InputText>(</InputText>
-								<FormInput type="text" name="currentRoadEndY" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" name="currentRoadEndX" onChange={this.handleRoadInputChange}></FormInput>
+								<InputText> , </InputText>
+								<FormInput type="text" name="currentRoadEndY" onChange={this.handleRoadInputChange}></FormInput>
 								<InputText>)</InputText>
 							</FormItem>
 							<FormItem style={{marginBottom:"10px"}}>
 								<InputDescription>Time to Travel:</InputDescription>
-								<FormInput type="text" name="currentRoadWeight" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" name="currentRoadWeight" onChange={this.handleRoadInputChange}></FormInput>
 								<InputText> minutes</InputText>
 							</FormItem>
 							{RoadFormButtons}
@@ -334,24 +484,25 @@ class CreateMapPage extends Component {
 						<FormTitle>Insert Location</FormTitle>
 							<FormItem style={{marginBottom:"10px"}}>
 								<InputDescription>Road Name:</InputDescription>
-								<FormInput type="text" boxWidth="200px" name="currentLocationRoadName" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" boxWidth="200px" name="currentLocationRoadName" onChange={this.handleLocationInputChange}></FormInput>
 							</FormItem>
-							<FormItem style={{marginBottom:"10px"}}>
+							<FormItem style={{marginBottom:"10px"}} display={this.state.currentLocationRoadNameValid ? "" : "none"}>
 								<InputDescription>Location Name:</InputDescription>
-								<FormInput type="text" boxWidth="200px" name="currentLocationName" onChange={this.handleInputChange}></FormInput>
+								<FormInput type="text" boxWidth="200px" name="currentLocationName" onChange={this.handleLocationInputChange}></FormInput>
 							</FormItem>
-							<FormItem style={{marginBottom:"10px"}}>
+							<FormItem style={{marginBottom:"10px"}} display={this.state.currentLocationRoadNameValid ? "" : "none"}>
 								<InputDescription>Coordinates (X,Y):</InputDescription>
 								<InputText>(</InputText>
-								<FormInput type="text" name="currentLocationX" onChange={this.handleInputChange}></FormInput>
-								<InputText>) , </InputText>
-								<InputText>(</InputText>
-								<FormInput type="text" name="currentLocationY" onChange={this.handleInputChange}></FormInput>
-								<InputText>)</InputText>
+								<InputText id="currentLocationX" color="orange">{this.state.currentLocationX.toFixed(2)}</InputText>
+								<InputText> , </InputText>
+								<InputText id="currentLocationY" color="orange">{this.state.currentLocationY.toFixed(2)}</InputText>
+								<InputText style={{marginRight:"10px"}}>)</InputText>
+								<svg height="34" width="14">
+									<polygon points="0,14 14,14 7,0" style={{fill:"orange"}} onClick={this.increaseLocationCoordinate} />
+									<polygon points="0,20 14,20 7,34" style={{fill:"orange"}} onClick={this.decreaseLocationCoordinate}/>
+								</svg>
 							</FormItem>
-							<FormItem>
-								<Button onClick={this.handleInsertLocationSubmit}>Insert Location</Button>
-							</FormItem>
+							{LocationFormButtons}
 						</InsertLocationForm>
 					</InsertLocationContainer>
 				</InsertionContainer>
@@ -367,7 +518,6 @@ class CreateMapPage extends Component {
 			</PageContainer>
 		);
 	}
-
 }
 
 export default CreateMapPage;
